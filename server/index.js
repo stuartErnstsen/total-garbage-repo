6e706e62e60742df814bd15684b6ce3dd4a934dd
stuartErnstsen <stuarternstsen@gmail.com>
2021-09-23 12:08:24 -0600

    added css and app.use(express.static)

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const path = require('path')
 
 const app = express()
 
-console.log(__dirname)
+
+app.use(express.static("client"));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 const port = process.env.PORT || 4400
